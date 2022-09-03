59ff598bfe0b9b350ead1e21fe5cc140900db51f
xunfeng <xunfeng1980@gmail.com>
2022-09-04 01:19:39 +0800

    1. add py03-demo

---
+++ crates/pyo3-demo/src/main.rs
@@ -0,0 +1,35 @@
+use pyo3::prelude::*;
+use pyo3::types::PyTuple;
+
+fn main() -> PyResult<()> {
+    let arg1 = "arg1";
+    let arg2 = "arg2";
+    let arg3 = "arg3";
+
+    Python::with_gil(|py| {
+        let fun: Py<PyAny> = PyModule::from_code(
+            py,
+            "def example(*args, **kwargs):
+                if args != ():
+                    print('called with args', args)
+                if kwargs != {}:
+                    print('called with kwargs', kwargs)
+                if args == () and kwargs == {}:
+                    print('called with no arguments')",
+            "",
+            "",
+        )?.getattr("example")?.into();
+
+        // call object without any arguments
+        fun.call0(py)?;
+
+        // call object with PyTuple
+        let args = PyTuple::new(py, &[arg1, arg2, arg3]);
+        fun.call1(py, args)?;
+
+        // pass arguments as rust tuple
+        let args = (arg1, arg2, arg3);
+        fun.call1(py, args)?;
+        Ok(())
+    })
+}
