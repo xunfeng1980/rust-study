0131eff0ead70bcca7e54b0672b3227a8e031628
xunfeng <xunfeng1980@gmail.com>
2022-09-09 15:44:43 +0800

    1. print predict time

--- crates/rust-bert-demo/src/main.rs
+++ crates/rust-bert-demo/src/main.rs
@@ -1,13 +1,16 @@
+use std::time::Instant;
 use rust_bert::pipelines::question_answering::{QaInput, QuestionAnsweringModel};
 
 fn main() {
     let qa_model = QuestionAnsweringModel::new(Default::default()).unwrap();
     let question = String::from("Where does Amy live ?");
     let context = String::from("Amy lives in Amsterdam");
-    let answers = qa_model.predict(&[QaInput { question, context }], 1, 32);
 
+    let now = Instant::now();
+    let answers = qa_model.predict(&[QaInput { question, context }], 1, 32);
     for answer in answers {
         println!("Answer: {:?}", answer);
     }
+    println!("执行时间: {}", now.elapsed().as_millis());
 }
 
